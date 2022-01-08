093756b9c180c1442d46e6b405aeb86908f0b4f3
Bruno Petito <brunopetito97@gmail.com>
2022-01-08 14:27:39 -0300

    modal refatorado com o class

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSmooth from './modules/scroll-smooth.js'
 import initAnimationScroll from './modules/animation-scroll.js'
 import Accordion from './modules/arccodion.js'
 import TabNav from './modules/tab-nav.js'
-import initModal from './modules/modal.js'
+import Modal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
@@ -22,8 +22,14 @@ const tabNav = new TabNav(
 )
 tabNav.init()
 
+const modal = new Modal(
+  '[data-modal= "abrir"]',
+  '[data-modal= "fechar"]',
+  '[data-modal= "container"]'
+)
+modal.init()
+
 initAnimationScroll()
-initModal()
 initTooltip()
 initDropdownMenu()
 initMenuMobile()
